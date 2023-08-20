517ba60cc658c03645c332c67b961b94a66383c5
Mike Fitzgibbon <gmf3312@gmail.com>
2023-08-20 17:27:00 -0500

    Delete src directory

--- src/main/java/mikefitzgibbon/launchcode/ConsoleDisplay.java
+++
@@ -1,9 +0,0 @@
-package mikefitzgibbon.launchcode;
-
-public class ConsoleDisplay extends TextArea{
-    @SuppressWarnings("CallToPrintStackTrace")
-    public ConsoleDisplay(){
-        super(10,40);
-        setEditable(false);
-    }
-}
--- src/main/java/mikefitzgibbon/launchcode/ConsoleField.java
+++
@@ -1,9 +0,0 @@
-package mikefitzgibbon.launchcode;
-
-import javax.swing.JTextField;
-
-public class ConsoleField extends JTextField{
-    public ConsoleField(){
-        setColumns(20);
-    }
-}
